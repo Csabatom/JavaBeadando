3434adbe8593560caeb5df1c8ce76699ad51f6a0
Nyirom2 <nyirocsabi@gmail.com>
2020-10-20 18:08:02 +0200

    Adattagok létrehozva

--- src/toto/tarolo/Fordulo.java
+++ src/toto/tarolo/Fordulo.java
@@ -1,4 +1,14 @@
 package toto.tarolo;
 
+import java.time.LocalDate;
+
 public class Fordulo {
+    private int ev;
+    private int het;
+    private int forduloAHeten;
+    private LocalDate datum;
+
+    public Fordulo() {
+
+    }
 }
--- src/toto/tarolo/Talalat.java
+++ src/toto/tarolo/Talalat.java
@@ -1,4 +1,7 @@
 package toto.tarolo;
 
 public class Talalat {
+    private int talalatokSzama;
+    private int nyertTalalatokSzama;
+    private int nyeremeny;
 }
